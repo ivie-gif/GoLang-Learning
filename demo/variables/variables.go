2c07ebb3db8bddb74e237fe162a572efd4b1ede9
Ivie Maryann Emwiongbon <emwiongbon_ivie@yahoo.com>
2025-02-10 15:42:24 +0100

    vairable and goLang classwork

--- demo/variables/variables.go
+++ demo/variables/variables.go
@@ -4,6 +4,23 @@ import "fmt"
 
 func main() {
 	var myName string = "Ivie"
-
 	fmt.Println("This is my Name:", myName, "Maryann")
+
+	userName := "Emwiongbon"
+	fmt.Println("My surname is:", userName)
+
+	partOne, partTwo := "james", "Admin"
+	fmt.Println(partOne, partTwo)
+
+	partTwo, Default := "mary", "Guest"
+	fmt.Println(partTwo, Default)
+
+	var (
+		lessonName string = "Variables"
+		lessonClass int = 12
+		lessonDuration float32 = 3.5
+		fee float32 = 1000
+	)
+
+	fmt.Println( "Lesson Name:", lessonName, "Class Age Grade:", lessonClass, "Duration:", lessonDuration, "Fee:", fee)
 }
--- demo/variadics/variadics.go
+++ demo/variadics/variadics.go
@@ -3,7 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	var myName string = "Ivie"
 
-	fmt.Println("This is my Name:", myName, "Maryann")
 }
